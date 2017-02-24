9279e657b0bc8ad30c16920cad00d6cb96eea116
neo1218 <neo1218@yeah.net>
2017-02-25 01:40:37 +0800

    share cookiejar:)

--- service/spider.py
+++ service/spider.py
@@ -4,10 +4,12 @@ import aiohttp
 info_login_url = "http://portal.ccnu.edu.cn/loginAction.do"
 
 async def info_login(sid, pwd):
+    _cookie_jar = None
     payload = {'userName': sid, 'userPass': pwd}
     async with aiohttp.ClientSession() as session:
         async with session.post(info_login_url, data=payload) as resp:
             resp_text = await resp.text()
             if resp_text.split('"')[1] == 'index_jg.jsp':
-                return session, sid
+                _cookie_jar = session.__dict__.get('_cookie_jar')
+                return _cookie_jar, sid
             else: return (None, sid)
