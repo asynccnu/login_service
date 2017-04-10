d273c32f5f67321c19d4e83102ecc68ecf3e50fb
neo1218 <neo1218@yeah.net>
2017-04-10 11:45:29 +0800

    return cookie dict

--- service/api.py
+++ service/api.py
@@ -6,7 +6,12 @@ api = web.Application()
 # ====== async view handlers ======
 @require_info_login
 async def info_login_api(request, s, sid):
-    return web.json_response({})
+    cookies = str(s.__dict__.get('_cookies').get('portal.ccnu.edu.cn'))
+    BIGipServerpool_portal = cookies.split()[1].split('=')[1][:-1]
+    JSESSIONID = cookies.split()[-3].split('=')[1][:-1]
+    return web.json_response({'cookie': {
+        'BIGipServerpool_portal': BIGipServerpool_portal,
+        'JSESSIONID': JSESSIONID }})
 # =================================
 
 # ====== url --------- maps  ======
