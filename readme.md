39e4e9ac8135a08111b6a754fe781b7c549a1819
neo1218 <neo1218@yeah.net>
2017-05-08 23:15:52 +0800

    tests pass

--- readme.md
+++ readme.md
@@ -4,3 +4,11 @@
 
 
 ## ``o(*≧▽≦)ツ`` 模拟登录现在在客户端, 不``＞(￣ε￣ = ￣3￣)<``要 封我``~\(≧▽≦)/>`` 
+
+## 测试
+
++ 配置```container.test.env```
+
+```shell
+$./start_test.sh && docker-compose -f docker-compose.test.yml logs --tail="100" mana_api_test
+```
--- start_test.sh
+++ start_test.sh
@@ -2,4 +2,4 @@
 
 docker-compose -f docker-compose.test.yml stop
 docker-compose -f docker-compose.test.yml build
-docker-compose -f docker-compose.test.yml up -d && docker-compose -f docker-compose.test.yml logs ele_api_test
+docker-compose -f docker-compose.test.yml up -d && docker-compose -f docker-compose.test.yml logs login_api_test
